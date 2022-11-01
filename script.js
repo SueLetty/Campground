b35fc518c0a5289cf059b733e7ff87f6c89a327d
SueLetty <xiuxiang1129@gmail.com>
2022-11-01 16:38:15 -0700

    implemented signup and login pages, and add click event listeners

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //code for getting user current coordinates
-let userCoords = []
-
 const onSuccess = (position) =>{
-    const userLat = position.coords.longitude;
-    const userLong = position.coords.latitude;
-    userCoords = [userLat, userLong]
-    console.log("Latitude:", userLat, "Longitude:", userLong);
+    const userLat = position.coords.latitude;
+    const userLong = position.coords.longitude;
+
 }
 
 const onError = (error) => {
@@ -14,36 +11,109 @@ const onError = (error) => {
     
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-// using city name and state name to get longitude and latitude
-form_btn.addEventListener("submit",(event) => {
+//signup button event
+// signup_form.addEventListener("submit", (event) => {
+function addUser(event){
     event.preventDefault();
 
-    //use this to call your API index 0 is latitude, index 1 is longitude
-    let destinationCoords = [];
-    const cityName = city.value;
-    const stateName = state.value;
-    let newCityName;
-
-    document.getElementById('form_btn').reset();
-
-    let coord_API = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=6a78d426e59589643788ea1b6371579f`;
-
-    if(cityName.includes(" ")){
-        newCityName = cityName.replace(" ", "%20");
-        const coord_API = `http://api.openweathermap.org/geo/1.0/direct?q=${newCityName}&limit=10&appid=6a78d426e59589643788ea1b6371579f`;
-
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const uname = document.getElementById("uname").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    
+    let currentList;
+            
+    if (window.localStorage.getItem("userLists")) {
+        currentList = JSON.parse(window.localStorage.getItem("userLists"));
+        console.log(currentList)
+    } else {
+        currentList = [];
     }
-    fetch(coord_API)
-    .then((res) => res.json())
-    .then((data) => {
-        for(const result of data){
-            if(result["name"] === cityName && result["state"] === stateName){
-                destinationCoords[0] = result["lat"];
-                destinationCoords[1] = result["lon"];
+    
+    const userData = {
+        fName: fname,
+        lName: lname,
+        uName: uname,
+        password: password,
+        Email: email,
+    }
+    
+    currentList.push(userData);
+    window.localStorage.setItem("userLists", JSON.stringify(currentList));
+    window.location = "login.html";
+         
+}
+
+// login button event
+// login_form.addEventListener("submit", (event) => {
+    function loginEvent(event){
+    event.preventDefault();
+    
+    let userList = JSON.parse(window.localStorage.getItem("userLists"));
+    console.log(userList);
+    const uname = document.getElementById("username_login").value;
+    const password = document.getElementById("password_login").value;
+    
+    debugger;
+    for(const i = 0; i < userList.length; i++){
+        const name = userList[i].fName;
+        
+        if(userList[i].uName == uname){
+            if(userList[i].passWord == password){
                 
+                window.location = "index.html";
+            }
+            else{
+                alert("Your User name does not match your password!");
+            }
+        } 
+        if(userList[i].password == password){
+            if(userList[i].uName == uname){
+                window.location = "index.html";
+            }
+            else{
+                alert("Your User name does not match your password!");
             }
         }
-    })
-    console.log(destinationCoords);
-})
+    }
+    const greeting = document.querySelector("#menu").createElement("p");
+    
+}
+
+// using city name and state name to get longitude and latitude
+// form_btn.addEventListener("submit",(event) => {
+//     event.preventDefault();
+
+//     //use this to call your API index 0 is latitude, index 1 is longitude
+//     // var destinationCoords = [];
+//     const cityName = city.value;
+//     const stateName = state.value;
+//     let newCityName;
+//     let destinationLat;
+//     let destinationLon;
+
+//     document.getElementById('form_btn').reset();
+
+//     let coord_API = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=6a78d426e59589643788ea1b6371579f`;
+
+//     if(cityName.includes(" ")){
+//         newCityName = cityName.replace(" ", "%20");
+//         coord_API = `http://api.openweathermap.org/geo/1.0/direct?q=${newCityName}&limit=10&appid=6a78d426e59589643788ea1b6371579f`;
+
+//     }
+//     fetch(coord_API)
+//     .then((res) => res.json())
+//     .then(async(data) => {
+//         for(const result of data){
+//             if(result["name"] === cityName && result["state"] === stateName){
+//                 destinationLat = result["lat"]
+//                 destinationLon = result["lon"];
+                
+//             }
+//         }
+//     });
+// })
+
+
 
