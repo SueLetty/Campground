79079b71dd3242c43c468bfddaecef401112c42c
gmejia3 <gemejiajr@gmail.com>
2022-11-01 22:09:46 -0700

    Added a div "row" underneath the div "container".

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//code for getting user current coordinates
+let userCoords = []
+
+const onSuccess = (position) =>{
+    const userLat = position.coords.longitude;
+    const userLong = position.coords.latitude;
+    userCoords.push(userLat, userLong);
+    console.log("Latitude:", userLat, "Longitude:", userLong);
+}
+console.log(userCoords);
+
+const onError = (error) => {
+    console.error(error);
+}
+    
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 // using city name and state name to get longitude and latitude
 form_btn.addEventListener("submit",(event) => {
@@ -18,8 +34,6 @@ form_btn.addEventListener("submit",(event) => {
         const coord_API = `http://api.openweathermap.org/geo/1.0/direct?q=${newCityName}&limit=10&appid=6a78d426e59589643788ea1b6371579f`;
 
     }
-
-
     fetch(coord_API)
     .then((res) => res.json())
     .then((data) => {
@@ -30,11 +44,86 @@ form_btn.addEventListener("submit",(event) => {
                 
             }
         }
-    })
-    console.log(destinationCoords);
+  
 
+        //Code to remove carousel
+        document.querySelector(".carousel").remove();
 
+        //Fetch to get the time with the coordinates.
+        let userUrl = `https://api.ipgeolocation.io/timezone?apiKey=8ae13e06e4a146dbb9bc8ee8617ed910&lat=${destinationCoords[0]}&long=${destinationCoords[1]}`;
+        
+        fetch(userUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            
+            const destinationTime = data.date_time;
 
-})
+            //Creation of the left side coloumn.
+            leftSide = document.createElement("div");
+            leftSide.classList.add("col-md-2");
+            leftSide.textContent = "Input information here";
+            leftSide.style.color = "white";                                            //Placeholder for visuals 
+            leftSide.style.paddingBottom = "600px";
+            leftSide.style.border = "5px solid red";
+            document.querySelector(".row").appendChild(leftSide);
 
+            //Creation of the div within the left side column.
+            timeDiv = document.createElement("div");
+            timeDiv.setAttribute("id", "time_div");
+            timeDiv.style.border = "5px solid white";                                  //Placeholder for visuals             
+            timeDiv.textContent = destinationTime + " [Destinaion Weather Variable]";  //Place Weather variable here
+            document.querySelector(".col-md-2").appendChild(timeDiv);
 
+            //Creation of the right side div which will hold the cards.
+            rightSide = document.createElement("div");
+            rightSide.classList.add("col-md-10");
+            rightSide.style.color = "white";                                           //Placeholder for visuals  
+            rightSide.textContent = "[Destinaions Cards]";
+            rightSide.style.paddingBottom = "600px";
+            rightSide.style.border = "5px solid red";
+            document.querySelector(".row").appendChild(rightSide);
+            
+            //Creation of cards
+            cardTemplate = document.createElement("div");
+            cardTemplate.classList.add("cardContainer")
+            cardTemplate.style.width = "18rem";
+            document.querySelector(".col-md-10").appendChild(cardTemplate);
+            
+            const cardImage = document.createElement("img");
+            cardImage.classList.add("card-img-top");
+            cardImage.setAttribute("src", "...");
+            cardImage.setAttribute("alt", "...");
+            document.querySelector(".cardContainer").appendChild(cardImage);
+
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body")
+            document.querySelector(".cardContainer").appendChild(cardBody);
+
+            const cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title");
+            cardTitle.textContent = "[Destination Title Variable]";
+            document.querySelector(".card-body").appendChild(cardTitle);
+
+            const cardParagraph = document.createElement("p");
+            cardParagraph.classList.add("card-text");
+            cardParagraph.textContent ="[This should be any dynamically pulled information from the campground api]";
+            document.querySelector(".card-body").appendChild(cardParagraph);
+
+            const linkTag = document.createElement("a");
+            linkTag.classList.add("btn");
+            linkTag.classList.add("btn-primary");
+            linkTag.setAttribute("href", "...");                        //This needs a source
+            linkTag.textContent = "More Information";
+            document.querySelector(".card-body").appendChild(linkTag);
+            //End of the card creation
+    
+        });
+
+    }) //For out fetch
+    
+}) //For addEventListener    
+
+//Code used to modify local current time.
+let currentDate = new Date();
+let currentTime = currentDate.getHours() + ":"  + currentDate.getMinutes();
+document.querySelector("#local_time").textContent =`Local Time: ${currentTime}`;
