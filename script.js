e275256c2ec5bf6c174b0b364b917c2169fa5f38
Anthony-Willis <anthonywillis1997@outlook.com>
2022-11-02 12:24:47 -0700

    Temp/weather bar and API, debug

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function addUser(event){
     const uname = document.getElementById("username_login").value;
     const password = document.getElementById("password_login").value;
     
-    for(const i = 0; i < userList.length; i++){
+    for(let i = 0; i < userList.length; i++){
         const name = userList[i].fName;
         
         if(userList[i].uName == uname){
@@ -95,12 +95,10 @@ function addEventToSearchBtn(event){
     event.preventDefault();
 
     //use this to call your API index 0 is latitude, index 1 is longitude
-    // var destinationCoords = [];
+    let destinationCoords = [];
     const cityName = city.value;
     const stateName = state.value;
     let newCityName;
-    let destinationLat;
-    let destinationLon;
 
     document.getElementById('form_btn').reset();
 
@@ -117,12 +115,68 @@ function addEventToSearchBtn(event){
     .then((data) => {
         for(const result of data){
             if(result["name"] === cityName && result["state"] === stateName){
-                destinationLat = result["lat"]
-                destinationLon = result["lon"];
+                destinationCoords[0] = result["lat"]
+                destinationCoords[1] = result["lon"];
                 
             }
         } 
-        let weatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLong}&appid=6a78d426e59589643788ea1b6371579f`;
+
+         //Code to remove carousel
+        document.querySelector(".carousel").remove();
+
+
+        //Creation of the left side coloumn.
+        leftSide = document.createElement("div");
+        leftSide.classList.add("col-md-2");
+        leftSide.textContent = cityName + ", " + stateName;
+        leftSide.style.color = "white";                                            //Placeholder for visuals 
+        leftSide.style.paddingBottom = "600px";
+        document.querySelector(".row").appendChild(leftSide);
+
+        //Creation of the right side div which will hold the cards.
+        rightSide = document.createElement("div");
+        rightSide.classList.add("col-md-10");
+        rightSide.style.color = "white";                                           //Placeholder for visuals  
+        rightSide.textContent = "[Destinaions Cards]";
+        rightSide.style.paddingBottom = "600px";
+        rightSide.style.border = "5px solid red";
+        document.querySelector(".row").appendChild(rightSide);
+        
+        //Creation of cards
+        cardTemplate = document.createElement("div");
+        cardTemplate.classList.add("cardContainer")
+        cardTemplate.style.width = "18rem";
+        document.querySelector(".col-md-10").appendChild(cardTemplate);
+        
+        const cardImage = document.createElement("img");
+        cardImage.classList.add("card-img-top");
+        cardImage.setAttribute("src", "...");
+        cardImage.setAttribute("alt", "...");
+        document.querySelector(".cardContainer").appendChild(cardImage);
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body")
+        document.querySelector(".cardContainer").appendChild(cardBody);
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent = "[Destination Title Variable]";
+        document.querySelector(".card-body").appendChild(cardTitle);
+
+        const cardParagraph = document.createElement("p");
+        cardParagraph.classList.add("card-text");
+        cardParagraph.textContent ="[This should be any dynamically pulled information from the campground api]";
+        document.querySelector(".card-body").appendChild(cardParagraph);
+
+        const linkTag = document.createElement("a");
+        linkTag.classList.add("btn");
+        linkTag.classList.add("btn-primary");
+        linkTag.setAttribute("href", "...");                        //This needs a source
+        linkTag.textContent = "More Information";
+        document.querySelector(".card-body").appendChild(linkTag);
+          //End of the card creation
+
+        let weatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${destinationCoords[0]}&lon=${destinationCoords[1]}&appid=6a78d426e59589643788ea1b6371579f`;
         const kelvin = 273;
 
         // Calling the API
@@ -131,11 +185,15 @@ function addEventToSearchBtn(event){
         .then((data) => {
             console.log(data);
             let temperature = Math.floor(data["main"]["temp"] - kelvin) *1.8 + 32;
+            let lowTemp = Math.floor(data["main"]["temp_min"] - kelvin) *1.8 + 32;
+            let highTemp = Math.floor(data["main"]["temp_max"] - kelvin) *1.8 + 32;
             //insert thiz into the left bracket 
+             //Creation of the div within the left side column.
+             weatherDiv = document.createElement("div");
+             weatherDiv.setAttribute("id", "weather_div");             
+             weatherDiv.textContent = "Current Temp: " + temperature + "°F" + "          Low: " + lowTemp + "°F" + "\n High: " + highTemp + "°F";  //Place Weather variable here
+             document.querySelector(".col-md-2").appendChild(weatherDiv);
         });
-
-        //Code to remove carousel
-        document.querySelector(".carousel").remove();
 
         //Fetch to get the time with the coordinates.
         let userUrl = `https://api.ipgeolocation.io/timezone?apiKey=8ae13e06e4a146dbb9bc8ee8617ed910&lat=${destinationCoords[0]}&long=${destinationCoords[1]}`;
@@ -146,66 +204,13 @@ function addEventToSearchBtn(event){
             
             const destinationTime = data.date_time;
 
-            //Creation of the left side coloumn.
-            leftSide = document.createElement("div");
-            leftSide.classList.add("col-md-2");
-            leftSide.textContent = "Input information here";
-            leftSide.style.color = "white";                                            //Placeholder for visuals 
-            leftSide.style.paddingBottom = "600px";
-            leftSide.style.border = "5px solid red";
-            document.querySelector(".row").appendChild(leftSide);
-
             //Creation of the div within the left side column.
             timeDiv = document.createElement("div");
-            timeDiv.setAttribute("id", "time_div");
-            timeDiv.style.border = "5px solid white";                                  //Placeholder for visuals             
-            timeDiv.textContent = destinationTime + " [Destinaion Weather Variable]";  //Place Weather variable here
+            timeDiv.setAttribute("id", "time_div");            
+            timeDiv.textContent = destinationTime;  //Place Weather variable here
             document.querySelector(".col-md-2").appendChild(timeDiv);
-
-            //Creation of the right side div which will hold the cards.
-            rightSide = document.createElement("div");
-            rightSide.classList.add("col-md-10");
-            rightSide.style.color = "white";                                           //Placeholder for visuals  
-            rightSide.textContent = "[Destinaions Cards]";
-            rightSide.style.paddingBottom = "600px";
-            rightSide.style.border = "5px solid red";
-            document.querySelector(".row").appendChild(rightSide);
-            
-            //Creation of cards
-            cardTemplate = document.createElement("div");
-            cardTemplate.classList.add("cardContainer")
-            cardTemplate.style.width = "18rem";
-            document.querySelector(".col-md-10").appendChild(cardTemplate);
-            
-            const cardImage = document.createElement("img");
-            cardImage.classList.add("card-img-top");
-            cardImage.setAttribute("src", "...");
-            cardImage.setAttribute("alt", "...");
-            document.querySelector(".cardContainer").appendChild(cardImage);
-
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body")
-            document.querySelector(".cardContainer").appendChild(cardBody);
-
-            const cardTitle = document.createElement("h5");
-            cardTitle.classList.add("card-title");
-            cardTitle.textContent = "[Destination Title Variable]";
-            document.querySelector(".card-body").appendChild(cardTitle);
-
-            const cardParagraph = document.createElement("p");
-            cardParagraph.classList.add("card-text");
-            cardParagraph.textContent ="[This should be any dynamically pulled information from the campground api]";
-            document.querySelector(".card-body").appendChild(cardParagraph);
-
-            const linkTag = document.createElement("a");
-            linkTag.classList.add("btn");
-            linkTag.classList.add("btn-primary");
-            linkTag.setAttribute("href", "...");                        //This needs a source
-            linkTag.textContent = "More Information";
-            document.querySelector(".card-body").appendChild(linkTag);
-            //End of the card creation
-    
         });
+
 
     }) //For out fetch
     
